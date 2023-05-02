a2e409a6ba72140abd4c7c1478fda16a869626d6
Theigrams <sy1909148@buaa.edu.cn>
2023-05-02 19:32:31 +0800

    change(ambient)

--- src/materials.py
+++ src/materials.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class Material:
-    def __init__(self, ambient=0.05, diffuse=1, specular=1, color=np.array([1, 1, 1])):
+    def __init__(self, ambient=0.01, diffuse=1, specular=1, color=np.array([1, 1, 1])):
         self.ambient = ambient
         self.diffuse = diffuse
         self.specular = specular
