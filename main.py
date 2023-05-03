9d9ecd147975d7bf972bcdf98e95186a05f4710d
Theigrams <sy1909148@buaa.edu.cn>
2023-05-03 21:16:07 +0800

    add(ray_tracing)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     sence1 = Sence(camera=camera1, objects=objects1, lights=lights1, canvas=canvas1)
     t1 = time.time()
     # sence1.render(samples_per_pixel=4, max_depth=1)
-    sence1.render_parallel(samples_per_pixel=4, max_depth=1, num_workers=7)
+    sence1.render_parallel(samples_per_pixel=4, max_depth=10, num_workers=7)
     t2 = time.time()
     canvas1.regular()
     print(f"Render time: {t2-t1}")
