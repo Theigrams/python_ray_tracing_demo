361ae3d66fd2679af00f02959431a14ed20906a8
Theigrams <sy1909148@buaa.edu.cn>
2023-05-05 19:42:19 +0800

    add(animate)

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from assets.cornell_box import *
 from src import *
 
 if __name__ == "__main__":
-    canvas1 = Canvas(400, 400)
+    canvas1 = Canvas(200, 200)
     camera1 = Camera()
     sence1 = Sence(camera=camera1, objects=objects1, lights=lights1, canvas=canvas1)
     t1 = time.time()
-    # sence1.render(samples_per_pixel=4, max_depth=1)
     plt.ion()
     plt.figure(1)
-    N = 100
-    t = np.linspace(0, 20, 100)
+    N = 20
     for i in tqdm(range(1, N + 1)):
         sence1.render_parallel(samples_per_pixel=4, max_depth=10, num_workers=7)
-        plt.imshow(np.sqrt(np.clip(canvas1.pixels / i, 0, 1)))
-        plt.pause(1)
+        img = np.sqrt(np.clip(canvas1.pixels / i, 0, 1))
+        plt.imshow(img)
+        plt.pause(0.01)
+        plt.imsave(f"output/{i}.png", img)
         plt.clf()
     t2 = time.time()
     canvas1.pixels /= N
     canvas1.regular()
     print(f"Render time: {t2-t1}")
     canvas1.save("output/image.png")
+    animate("output", "output/animation.gif", 5)
